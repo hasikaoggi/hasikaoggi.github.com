88eff95661a6ff59b9ec1c4245939574d917b13f
surya <spadala@evoketechnologies.com>
2023-09-24 18:06:08 +0530

    fixed the issues with header nav buttons

--- custom.js
+++ custom.js
@@ -1,38 +1,27 @@
-// Get references to the navbar-toggler button and the collapse element
 const navbarCollapse = document.getElementById("navbarNavAltMarkup");
-// Add a click event listener to each navigation item
 const navItems = navbarCollapse.querySelectorAll(".nav-link");
 navItems.forEach((navItem) => {
   navItem.addEventListener("click", (e) => {
-    // Remove active class from all nav links
     navItems.forEach((link) => {
-      link.classList.remove("active");
+     link.classList.remove("active");
     });
-
-    // Add active class to clicked nav link
-    e.target.classList.add("active");
-
-    if (navbarCollapse.classList.contains("show")) {
-      navbarCollapse.classList.remove("show");
-    }
+    e.currentTarget.classList.add("active");
   });
 });
 
-document.addEventListener("scroll", function () {
-  let fromTop = window.scrollY;
 
+document.addEventListener("scroll", function () {
+  let fromTop = window.scrollY+1;
   navbarCollapse.querySelectorAll(".nav-link").forEach((link) => {
     let section = document.querySelector(link.hash);
-
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
+   if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
       navItems.forEach((link) => {
         link.classList.remove("active");
       });
-
       link.classList.add("active");
     }
   });
 });
+
+
+
