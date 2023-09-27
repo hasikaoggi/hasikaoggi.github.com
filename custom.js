e456d048155a8632e22707ecc69b0f7b9ad81e83
satya kumar <rambalasatya97@gmail.com>
2023-09-27 12:34:26 +0530

    made image large on click

--- custom.js
+++ custom.js
@@ -25,6 +25,28 @@ document.addEventListener("scroll", function () {
     }
   });
 });
+ 
+// Function to open the modal and display the image
+function openModal() {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+  modal.style.display = "block";
+  modalImg.src = "images/ai.jpg";
+}
+
+// Function to close the modal
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
 
 
 
